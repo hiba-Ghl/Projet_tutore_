1390e17c062b151dbc7cba53436506648678befb
hiba-Ghl <115666230+hiba-Ghl@users.noreply.github.com>
2024-12-09 00:19:37 +0100

    ajouter un formulair de donneur

--- KineBook/KineBook-project/src/app/app.routes.ts
+++ KineBook/KineBook-project/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DonneurFormComponent } from './donneur-form/donneur-form.component';  // Assurez-vous d'importer le bon composant
+
 
 export const routes: Routes = [{path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' } 
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'don-form', component: DonneurFormComponent },
+
 ];
