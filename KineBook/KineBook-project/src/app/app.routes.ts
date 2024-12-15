5dd3fefc1d83a231507c3eef8f0b99751f4399e7
hiba-Ghl <115666230+hiba-Ghl@users.noreply.github.com>
2024-12-15 19:29:24 +0100

    developper mon code

--- KineBook/KineBook-project/src/app/app.routes.ts
+++ KineBook/KineBook-project/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DonneurFormComponent } from './donneur-form/donneur-form.component';  // Assurez-vous d'importer le bon composant
 import { KineSignUpComponent } from './kine-sign-up/kine-sign-up.component';
-
+import { SessionKineComponent } from './session-kine/session-kine.component';
 
 
 export const routes: Routes = [{path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'don-form', component: DonneurFormComponent },
   { path: 'sign-up', component: KineSignUpComponent},
-
+  { path: 'session-Kine', component: SessionKineComponent}
 ];
