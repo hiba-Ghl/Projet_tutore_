6fa1547f9e5f35c8074cd90d734eacdceca7aa61
hiba-Ghl <115666230+hiba-Ghl@users.noreply.github.com>
2024-12-15 17:35:48 +0100

    page principale presque terminé

---
+++ KineBook/KineBook-project/src/app/session-kine/session-kine.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-session-kine',
+  standalone: true,
+  imports: [],
+  templateUrl: './session-kine.component.html',
+  styleUrl: './session-kine.component.css'
+})
+export class SessionKineComponent {
+
+}
