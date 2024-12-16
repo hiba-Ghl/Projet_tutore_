cb7534a705f7ff886331af32f52e7a1cc33970f6
hiba-Ghl <115666230+hiba-Ghl@users.noreply.github.com>
2024-12-16 02:34:15 +0100

    dev des offres dans session-kine

--- KineBook/KineBook-project/src/app/app.routes.ts
+++ KineBook/KineBook-project/src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DonneurFormComponent } from './donneur-form/donneur-form.component';
 import { KineSignUpComponent } from './kine-sign-up/kine-sign-up.component';
+import { SidebarKineComponent } from './sidebar-kine/sidebar-kine.component';
 import { SessionKineComponent } from './session-kine/session-kine.component';
-
+import { OffreKineComponent } from './offre-kine/offre-kine.component';
 
 export const routes: Routes = [{path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'don-form', component: DonneurFormComponent },
   { path: 'sign-up', component: KineSignUpComponent},
-  { path: 'session-Kine', component: SessionKineComponent}
+  { path: 'sidebar', component: SidebarKineComponent},
+  { path: 'session-Kine', component: SessionKineComponent},
+  { path: 'offre', component: OffreKineComponent}
 ];
--- KineBook/KineBook-project/src/app/session-kine/session-kine.component.ts
+++ KineBook/KineBook-project/src/app/session-kine/session-kine.component.ts
@@ -1,10 +1,11 @@
 import { Component} from '@angular/core';
+import { SidebarKineComponent } from "../sidebar-kine/sidebar-kine.component";
 
 
 @Component({
   selector: 'app-session-kine',
   standalone: true,
-  imports: [],  // Ajouter CommonModule ici
+  imports: [SidebarKineComponent],  // Ajouter CommonModule ici
   templateUrl: './session-kine.component.html',
   styleUrls: ['./session-kine.component.css']
 })
