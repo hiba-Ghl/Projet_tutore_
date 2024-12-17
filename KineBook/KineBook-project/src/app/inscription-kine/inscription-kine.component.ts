e3553dba364b1f6d5f46ef383fc55dd392f677e3
JihaneTaif <jihane.taif99@gmail.com>
2024-12-17 14:56:42 +0100

    ajouter nouveau components

---
+++ KineBook/KineBook-project/src/app/inscription-kine/inscription-kine.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inscription-kine',
+  standalone: true,
+  imports: [],
+  templateUrl: './inscription-kine.component.html',
+  styleUrl: './inscription-kine.component.css'
+})
+export class InscriptionKineComponent {
+
+}
