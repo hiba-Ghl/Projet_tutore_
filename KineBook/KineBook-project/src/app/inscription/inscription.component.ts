7ab5f2db39670a782add913ade502acb70457501
hiba-Ghl <115666230+hiba-Ghl@users.noreply.github.com>
2024-12-18 01:27:41 +0100

    add nav dans l'inscription et rectifier some elts dans contact

--- KineBook/KineBook-project/src/app/inscription/inscription.component.ts
+++ KineBook/KineBook-project/src/app/inscription/inscription.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.css'
 })
