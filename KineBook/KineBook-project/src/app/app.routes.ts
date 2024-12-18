0e1fe6438fbe555486752b6c584e808a08aed9b7
JihaneTaif <jihane.taif99@gmail.com>
2024-12-18 13:01:36 +0100

    Save progress

--- KineBook/KineBook-project/src/app/app.routes.ts
+++ KineBook/KineBook-project/src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { SidebarKineComponent } from './sidebar-kine/sidebar-kine.component';
 import { SessionKineComponent } from './session-kine/session-kine.component';
 import { OffreKineComponent } from './offre-kine/offre-kine.component';
 import { CartesOffresComponent } from './cartes-offres/cartes-offres.component';
-<<<<<<< HEAD
 
 import { MesReservationPageComponent } from '../app/mes-reservation-page/mes-reservation-page.component';
 import { OffreFormComponent } from '../app/offre-form/offre-form.component';
@@ -22,12 +21,8 @@ import { InscriptionDonneurComponent } from './inscription-donneur/inscription-d
 import { InscriptionKineComponent } from './inscription-kine/inscription-kine.component';
 import { ShowReservationComponent } from './show-reservation/show-reservation.component';
 import { EditProfilKineComponent } from './edit-profil-kine/edit-profil-kine.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-=======
-import { MesReservationPageComponent } from './mes-reservation-page/mes-reservation-page.component';
-import { OffreFormComponent } from './offre-form/offre-form.component';
-import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
->>>>>>> b29613dc7f1f27aed9dbed110185af2757e3685c
 
 export const routes: Routes = [{path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -38,27 +33,20 @@ export const routes: Routes = [{path: 'dashboard', component: DashboardComponent
   { path: 'session-Kine', component: SessionKineComponent},
   { path: 'offre', component: OffreKineComponent},
   { path: 'carte-offre', component: CartesOffresComponent},
-<<<<<<< HEAD
-
-=======
->>>>>>> b29613dc7f1f27aed9dbed110185af2757e3685c
   { path: 'kine-sign-up', component: KineSignUpComponent},
   { path: 'user-sign-up', component: UserSignUpComponent},
   { path: 'mes-reservation-page', component: MesReservationPageComponent},
   { path: 'offre-form', component: OffreFormComponent},
 
-<<<<<<< HEAD
 
   { path: 'ajouter-offre', component:AjoutOffreComponent},
   { path: 'reservation-offre', component: MesReservationPageComponent},
-  { path: 'FAQ', component:FAQPageComponent},
   { path: 'contact', component: ContactPageComponent},
   { path: 'inscription',component:InscriptionComponent },
   { path: 'inscription-patient',component:InscriptionPatientComponent},
   { path: 'inscription-donneur',component:InscriptionDonneurComponent},
   { path: 'inscription-kine',component:InscriptionKineComponent},
   { path: 'reservation-patient',component:ShowReservationComponent},
-  { path: 'profil-kine',component:EditProfilKineComponent},
-=======
->>>>>>> b29613dc7f1f27aed9dbed110185af2757e3685c
+  { path: 'profil-kine',component:EditProfilKineComponent}, 
+  { path: 'sign-in',component:SignInComponent},
 ];
