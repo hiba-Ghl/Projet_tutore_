6038975c61a349d52a8baa790cd59f86dc72ca22
JihaneTaif <jihane.taif99@gmail.com>
2024-12-29 00:00:28 +0100

    new component dashboard-donneut

---
+++ KineBook/KineBook-project/src/app/dashboard-donneur/dashboard-donneur.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-donneur',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-donneur.component.html',
+  styleUrl: './dashboard-donneur.component.css'
+})
+export class DashboardDonneurComponent {
+
+}
